df98025c337df023473ded5a584adde0bfc4c450
Wannie18 <wanangwasiska@gmail.com>
2023-04-21 01:18:32 -0700

    Printing using puts in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
+/**
+ * This the main function where execution starts
+ */
 int main(void)
 {
 	char PrintStr[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(PrintStr);
-	return 0;
+	return (0);
 }
