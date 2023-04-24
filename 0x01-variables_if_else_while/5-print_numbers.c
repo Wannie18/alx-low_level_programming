23ac99dd64f127197256928229fbd749b1ae3173
Wannie18 <wanangwasiska@gmail.com>
2023-04-24 08:03:22 -0700

    print single digit numbers of base 10 starting with 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - This is where execution of the program starts from
+ *
+ * Return: 0 after completing execution
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
