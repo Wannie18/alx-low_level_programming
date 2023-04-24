7e37354f71b7ddc2d62c4370b32757d7bf9c9088
Wannie18 <wanangwasiska@gmail.com>
2023-04-24 13:58:24 -0700

    printing double digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - This is where execution of the program starts from
+ *
+ * Return: 0 after completing execution
+ */
+int main(void)
+{
+	int i = 1;
+
+	while (i < 10)
+	{
+		putchar('0');
+		putchar('0' + i);
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
