832805d4ce89d85b4c3e41bbd656a0c53f55d317
Wannie18 <wanangwasiska@gmail.com>
2023-04-24 12:46:33 -0700

    print digits with putchar without using a char type variable

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - This is where execution of the program starts from
+ *
+ * Return: 0 after completing execution
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar('0' + i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
