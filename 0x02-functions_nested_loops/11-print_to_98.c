053ccf4c4246c2eea15c242836c5aae7b53aade2
Wannie18 <wanangwasiska@gmail.com>
2023-04-26 15:28:23 -0700

    prints number sfrom n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,15 +8,11 @@ void  print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		int i;
-
 		for (i = n; n <= 98; n++)
 		       printf("%d, ", n);	
 	}
 	else
 	{
-		int a;
-
 		for (a = n; n <= 98; n--)
 			print("%d, ", n);
 	}
