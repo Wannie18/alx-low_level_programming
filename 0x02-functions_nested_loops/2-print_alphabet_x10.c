a8f4e9f5fb625751e16c3bf82c57367d74f6b2fc
Wannie18 <wanangwasiska@gmail.com>
2023-04-25 18:44:58 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet in small letters
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char i = 'a';
+	int a = 0;
+
+	while (a <= 10)
+	{
+		while (i <= 'z')
+		{
+			_putchar(i);
+			i++;
+		}
+	_putchar('\n');
+	}
+}
