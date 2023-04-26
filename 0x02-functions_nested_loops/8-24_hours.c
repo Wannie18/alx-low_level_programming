4cff7b0495ce055f468c2763427c3add78de61fb
Wannie18 <wanangwasiska@gmail.com>
2023-04-26 14:25:19 -0700

    print 24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,28 +14,29 @@ void jack_bauer(void)
 		while (m <= 9)
 		{
 			if (h == 2 && m > 3)
+			{
 				break;
-			else
-				int s = 0;
+			}
+			int s = 0;
 
-				while (s < 6)
-				{
-					int c = 0;
+			while (s < 6)
+			{
+				int c = 0;
 
-					while (c <= 9)
-					{	
-						_putchar('0' + h);
-						_putchar('0' + m);
-						_putchar(' ');
-						_putchar(':');
-						_putchar(' ');
-						_putchar('0' + s);
-						_putchar('0' + c);
-						_putchar('\n');
-						c++;
-					}
-					s++;
+				while (c <= 9)
+				{	
+					_putchar('0' + h);
+					_putchar('0' + m);
+					_putchar(' ');
+					_putchar(':');
+					_putchar(' ');
+					_putchar('0' + s);
+					_putchar('0' + c);
+					_putchar('\n');
+					c++;
 				}
+				s++;
+			}
 			m++;
 		}
 		h++;
