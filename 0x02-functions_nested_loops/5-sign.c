760890a27be19b4821440917211a2ad8e18d96fa
Wannie18 <wanangwasiska@gmail.com>
2023-04-25 19:54:42 -0700

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign  - prints the alphabet in small letters
+ * @n: interger to be evaluated
+ *
+ * Return: 1 if greater than zero, 0 if zero and -1 if anything else
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-')
+		return (-1);
+	{
+}
