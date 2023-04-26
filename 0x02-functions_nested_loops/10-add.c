713e9641326eb18f71227886582ce793e71bddfc
Wannie18 <wanangwasiska@gmail.com>
2023-04-26 14:37:07 -0700

    adds 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds 2 numbers and prints
+ * @n1: first number
+ * @n2: second number
+ *
+ * Return: sum
+ */
+int add(n1, n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+	return (sum);	
+}
