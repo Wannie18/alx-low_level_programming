c71f361581734c76625e50d972c60ca314e3e0d2
Wannie18 <wanangwasiska@gmail.com>
2023-05-02 16:32:11 -0700

    print every other number

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
  * puts2 - prints every other character in a string
- * 
+ *
  * @str: string received
  */
 void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != 0; i+=2)
+	for (i = 0; str[i] != 0; i += 2)
 	{
 		_putchar(str[i]);
 	}
+	_putchar('\n');
 }
