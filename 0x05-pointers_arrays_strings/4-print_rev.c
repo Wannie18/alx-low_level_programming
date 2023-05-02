c2c3061da696ebe28090a98ddce2b603c70417c2
Wannie18 <wanangwasiska@gmail.com>
2023-05-02 15:00:50 -0700

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints an array of charecters to the stout in reverse
+ * @s:  string to be printed
+ */
+void print_rev(char *s)
+{
+	int c = 0;
+	int i = 0;
+
+	while (s[c] != 0)
+	{
+		c++;
+	}
+	while (i <= c )
+	{
+		_putchar(s[c-i]);
+		i++;
+	}
+	_putchar('\n');
+}
