3dbfcdda7b7d401e0ab298a946281546c8f3a0c3
Wannie18 <wanangwasiska@gmail.com>
2023-05-02 17:50:52 -0700

    copying values of a string from one pointer into another

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strcpy - copies string from one pointer to another
+ *
+ * @dest: ponter to be copied to
+ * @src: pointer to copy from
+ *
+ */
+char * _strcpy(char *dest, char *src)
+{
+	*dest = *src;
+	return dest;
+}
