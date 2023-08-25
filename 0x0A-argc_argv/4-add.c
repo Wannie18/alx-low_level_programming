c8af9b40c8d21004f1962680e503f372a0722e15
Wannie18 <wanangwasiska@gmail.com>
2023-08-25 02:00:59 -0700

    multiplies two number and prints the result

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,8 +13,8 @@ int main(int argc, __attribute__((unused)) char *argv[])
 	if (argc > 2)
 	{
 		int num1 = atoi(argv[1]);
-		int num2 = atoi( argv[2]);
-		int result = num1 * num2;
+		int num2 =atoi( argv[2]);
+		int result = num1 + num2;
 
 		printf("%d\n", result);
 		return (0);
