36c60078517baedbfe5116a29832a2d6b373ffe7
Wannie18 <wanangwasiska@gmail.com>
2023-08-25 01:03:42 -0700

    prints all arguments including the first one

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints all the arguments passed to it
+ * @argc: holds the count of the arguments passed
+ * @argv: hold the arguments in an array of pointers
+ * Return: zero if successful
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
