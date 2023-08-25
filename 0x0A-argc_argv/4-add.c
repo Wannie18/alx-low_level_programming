39b1597ccbdbcce11af370c8d621c5fbb1f6708a
Wannie18 <wanangwasiska@gmail.com>
2023-08-25 02:51:53 -0700

    adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 /**
  * main - prints all the arguments passed to it
  * @argc: holds the count of the arguments passed
@@ -10,19 +12,35 @@
  */
 int main(int argc, __attribute__((unused)) char *argv[])
 {
-	if (argc > 2)
+	int i;
+	unsigned int k, sum = 0;
+	char *e;
+
+	if (argc > 1)
 	{
-		int num1 = atoi(argv[1]);
-		int num2 =atoi( argv[2]);
-		int result = num1 + num2;
+		for (i = 1; i < argc; i++)
+		{
+			e = argv[i];
 
-		printf("%d\n", result);
-		return (0);
+			for (k = 0; k < strlen(e); k++)
+			{
+				if (e[k] < 48 || e[k] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
 
+			sum += atoi(e);
+			e++;
+		}
+
+		printf("%d\n", sum);
 	}
 	else
 	{
-		printf("Error\n");
-		return (1);
+		printf("0\n");
 	}
+
+	return (0);
 }
