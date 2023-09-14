b72ad688c68724179d7bf3ed974ac0d25d00f988
Wannie18 <wanangwasiska@gmail.com>
2023-09-14 10:19:09 -0700

    program that prints the name of the file it was called from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the program it was called from
+ *
+ * Return : Always zero
+ *
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
